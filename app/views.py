42fccce64023131dfe39cf1936ca99c2eef2f4b3
Taylor Bigham <mtbigham@ufl.edu>
2015-11-05 14:35:25 -0800

    Added follow/unfollow capability

--- app/views.py
+++ app/views.py
@@ -67,6 +67,44 @@ def edit():
 		form.nickname.data = g.user.nickname
 		form.about_me.data = g.user.about_me
 	return render_template('edit.html', form=form)
+	
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+	user = User.query.filter_by(nickname=nickname).first()
+	if user is None:
+		flash('User %s not found.' % nickname)
+		return redirect(url_for('index'))
+	if user == g.user:
+		flash('You can\'t follow yourself!')
+		return redirect(url_for('user', nickname=nickname))
+	u = g.user.follow(user)
+	if u is None:
+		flash('Cannot follow '+nickname+'.')
+		return redirect(url_for('user', nickname=nickname))
+	db.session.add(u)
+	db.session.commit()
+	flash('You are now following '+nickname+'!')
+	return redirect(url_for('user', nickname=nickname))
+	
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+	user = User.query.filter_by(nickname=nickname).first()
+	if user is None:
+		flash('User %s not found.' % nickname)
+		return redirect(url_for('index'))
+	if user == g.user:
+		flash('You can\'t unfollow yourself!')
+		return redirect(url_for('user', nickname=nickname))
+	u = g.user.unfollow(user)
+	if u is None:
+		flash('Cannot unfollow '+nickname+'.')
+		return redirect(url_for('user', nickname=nickname))
+	db.session.add(u)
+	db.session.commit()
+	flash('You have stopped following '+nickname+'.')
+	return redirect(url_for('user', nickname=nickname))
 
 #TODO: Learn about how the before_request Flask event works
 @app.before_request
@@ -94,6 +132,9 @@ def after_login(resp):
 		nickname = User.make_unique_nickname(nickname)
 		user = User(nickname=nickname, email=resp.email)
 		db.session.add(user)
+		db.session.commit()
+		#make user follow themself
+		db.session.add(user.follow(user))
 		db.session.commit()
 	remember_me = False
 	if 'remember_me' in session:
